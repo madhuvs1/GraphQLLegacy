ac9c65808ffe7f58abdb4b3a640d85d1a3255c03
Madhu_VedhagiriSubra <madhu_vedhagirisubra@dell.com>
2024-05-31 15:23:48 -0500

    updates-entityframework and data seeding

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using GraphQL.Types;
 using GraphQL;
 using GraphiQl;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddTransient<IMenuRepository, MenuRepository>();
@@ -10,6 +11,7 @@ builder.Services.AddTransient<MenuMutation>();
 builder.Services.AddTransient<MenuInputType>();
 builder.Services.AddTransient<ISchema, MenuSchema>();
 builder.Services.AddGraphQL(b => b.AddAutoSchema<ISchema>().AddSystemTextJson());
+builder.Services.AddDbContext<GraphQLDbContext>(option => option.UseSqlite(builder.Configuration.GetConnectionString("GraphQLDbContextConnection")));
 var app = builder.Build();
 
 //app.MapGet("/", () => "Hello World!");
--- Services/MenuRespository.cs
+++ Services/MenuRespository.cs
@@ -1,8 +1,10 @@
-
-using System.Diagnostics.CodeAnalysis;
-
 public class MenuRepository : IMenuRepository
 {
+    private GraphQLDbContext dbContext;
+    public MenuRepository(GraphQLDbContext graphQLDbContext)
+    {
+        dbContext = graphQLDbContext;
+    }
   private static List<Menu> MenuList = new List<Menu>()
         {
             new Menu() { Id = 1, Name = "Classic Burger", Description="A juicy chicken burger with lettuce and cheese" , Price = 8.99},
@@ -15,28 +17,83 @@ public class MenuRepository : IMenuRepository
 
     public Menu AddMenu(Menu menu)
     {
-        MenuList.Add(menu);
+        dbContext.Menus.Add(menu);
+        dbContext.SaveChanges();
         return menu;
     }
 
     public void DeleteMenu(int id)
     {
-        MenuList.RemoveAt(id);
+        var menuResult = dbContext.Menus.Find(id);
+        dbContext.Menus.Remove(menuResult);
+        dbContext.SaveChanges();
     }
 
     public List<Menu> GetAllMenu()
     {
-        return  MenuList;
+        return  dbContext.Menus.ToList();
     }
 
     public Menu GetMenuById(int id)
     {
-        return MenuList.Find(m => m.Id == id);
+        return dbContext.Menus.Find(id);
     }
 
     public Menu UpdateMenu(int id, Menu menu)
     {
-        MenuList[id] = menu;
-        return menu;
+        var menuResult = dbContext.Menus.Find(id);
+        menuResult.Name = menu.Name;
+        menuResult.Description = menu.Description;
+        menuResult.Price = menu.Price;
+       dbContext.SaveChanges();
+       return menu;
     }
 }
+
+
+
+
+// public class MenuRepository : IMenuRepository
+// {
+//     private GraphQLDbContext dbContext;
+//     public MenuRepository(GraphQLDbContext graphQLDbContext)
+//     {
+//         dbContext = graphQLDbContext;
+//     }
+//   private static List<Menu> MenuList = new List<Menu>()
+//         {
+//             new Menu() { Id = 1, Name = "Classic Burger", Description="A juicy chicken burger with lettuce and cheese" , Price = 8.99},
+//             new Menu() { Id = 2, Name = "Margherita Pizza", Description = "Tomato, mozzarella, and basil pizza", Price = 10.50 },
+//             new Menu() { Id = 3, Name = "Grilled Chicken Salad", Description = "Fresh garden salad with grilled chicken", Price = 7.95 },
+//             new Menu() { Id = 4, Name = "Pasta Alfredo", Description = "Creamy Alfredo sauce with fettuccine pasta", Price = 12.75 },
+//             new Menu() { Id = 5, Name = "Chocolate Brownie Sundae", Description = "Warm chocolate brownie with ice cream and fudge", Price = 6.99 },
+
+//         };
+
+//     public Menu AddMenu(Menu menu)
+//     {
+//         MenuList.Add(menu);
+//         return menu;
+//     }
+
+//     public void DeleteMenu(int id)
+//     {
+//         MenuList.RemoveAt(id);
+//     }
+
+//     public List<Menu> GetAllMenu()
+//     {
+//         return  MenuList;
+//     }
+
+//     public Menu GetMenuById(int id)
+//     {
+//         return MenuList.Find(m => m.Id == id);
+//     }
+
+//     public Menu UpdateMenu(int id, Menu menu)
+//     {
+//         MenuList[id] = menu;
+//         return menu;
+//     }
+// }
